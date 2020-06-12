4cfa6419dbb34aaa99f13beb01afe8d023ebd3db
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-06-12 23:44:21 +0200

    Update game_message.js

---
+++ gws/commands/game_message.js
@@ -0,0 +1,13 @@
+/** 
+interface Input {
+  commandId       u8
+  receiver_id     u8
+  payload_to_forward     
+*/
+
+/** out
+interface Response {
+  commandId       u8
+  sender_id       u8
+  payload_to_forward       
+*/
