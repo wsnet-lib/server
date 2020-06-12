44415c5716ac8965f2889fbce09f477bc60204d6
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-06-13 01:12:32 +0200

    Update index.js

--- gws/commands/index.js
+++ gws/commands/index.js
@@ -1,15 +1,15 @@
 
 const commandIds = {
-  'lobby_list': 0,
-  'lobby_create': 1,
-  'lobby_join': 2,
-  'lobby_leave': 3,
-  'lobby_allow_join': 4,
-  'lobby_kick': 5,
-  'lobby_transfer': 6,
-  'lobby_max_players': 7,
-  'lobby_start_game': 8,
-  'game_message': 9
+  lobby_list: 0,
+  lobby_create: 1,
+  lobby_join: 2,
+  lobby_leave: 3,
+  lobby_allow_join: 4,
+  lobby_kick: 5,
+  lobby_transfer: 6,
+  lobby_max_players: 7,
+  lobby_start_game: 8,
+  game_message: 9
 }
 
 const commands = [
